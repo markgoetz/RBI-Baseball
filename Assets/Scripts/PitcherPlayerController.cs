869790d2ae3202cd969fc052cea7eb052aafca31
Mark Goetz <mark@markandrewgoetz.com>
2015-12-10 21:03:31 -0600

    Implemented dummy UI pitcher buttons
    The Pitching Scene now has UI buttons that, on click, cause the pitcher
    sprite to play the Throwing animation.

---
+++ Assets/Scripts/PitcherPlayerController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PitcherPlayerController : MonoBehaviour {
+
+	public PitcherSprite pitcher;
+
+	public void ThrowPitch(int pitch_number) {
+		pitcher.playThrowAnimation(pitch_number);
+	}
+}
---
+++ Assets/Scripts/PitcherSprite.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Animator))]
+public class PitcherSprite : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	public void playThrowAnimation (int pitch_number) {
+		anim.SetTrigger ("throwpitch");
+	}
+}
