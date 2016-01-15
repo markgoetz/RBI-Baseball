129e2592731da76f88973fc177a680d71ece2fa6
Mark Goetz <mark@markandrewgoetz.com>
2016-01-14 23:03:31 -0600

    Fixed pitching cursor hack
    Moved the pitching cursor into the UI to make it simpler, less tacky,
    and more true to size.

--- Assets/Scripts/View/PitchingCursor.cs
+++ Assets/Scripts/View/PitchingCursor.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent (typeof(RectTransform))]
+[RequireComponent (typeof(Image))]
 public class PitchingCursor : MonoBehaviour {
 	private FieldingUIController UIController;
 	private PitcherController pitcherController; // TODO: THIS NEEDS TO BE REFACTORED!!!!
 	private StrikeZoneView view;
 	private bool inside_strike_zone;
 	private bool visible;
-	private Rect rect;
+	private RectTransform transform;
 	
 	private float strike_zone_width;
 	
@@ -17,7 +19,7 @@ public class PitchingCursor : MonoBehaviour {
 		view = UIController.strikeZone;
 		strike_zone_width = view.pixelWidth;
 		pitcherController = PitcherController.getInstance();
-		rect = GetComponent<RectTransform>().rect;
+		transform = GetComponent<RectTransform>();
 	}
 	
 	// Move the fielding cursor so that it follows the mouse.
@@ -26,20 +28,14 @@ public class PitchingCursor : MonoBehaviour {
 	
 		Vector3 screenpoint = Input.mousePosition;
 	
-		transform.position = new Vector2(
-			Camera.main.ScreenToWorldPoint(screenpoint).x,
-			Camera.main.ScreenToWorldPoint(screenpoint).y
-		);
+		transform.position = screenpoint;
 		
 		InsideStrikeZone = view.isInsideStrikeZone(screenpoint);
 		
 		// Scale in relation to distance from the sweet spot
 		Vector2 strike_zone_location = view.worldSpaceToStrikeZone(screenpoint);
-		float radius = pitcherController.spreadRadius(strike_zone_location);
-		
-		float scale = getScale(radius);
-		
-		transform.localScale = new Vector2(scale, scale);
+		float radius = strike_zone_width * pitcherController.spreadRadius(strike_zone_location);
+		transform.sizeDelta = new Vector2(radius, radius);
 	}
 	
 	public void Clicked() {
@@ -53,31 +49,21 @@ public class PitchingCursor : MonoBehaviour {
 		get { return inside_strike_zone; }
 		set {
 			inside_strike_zone = value;
-			GetComponent<SpriteRenderer>().enabled = Enabled;
+			GetComponent<Image>().enabled = Enabled;
 		}
 	}
-	
-	private float getScale(float radius) {
-		// How wide is the strike zone in pixels?
-		// scale the radius until it matches the strike zone * radius.
-		float cursor_pixel_size = radius * strike_zone_width;
-		Debug.Log ("Radius: " + radius + "  Strike zone width: " + strike_zone_width + "  Cursor size:" + cursor_pixel_size);
-		
-		Debug.Log ("sprite size:" + rect.width + "   scale: " + (cursor_pixel_size * rect.width));
-		return cursor_pixel_size * rect.width / 100; // HACK: The 100 is hardcoded!
-	}
-	
+
 	public bool Visible {
 		get { return visible; }
 		set {
 			visible = value;
-			GetComponent<SpriteRenderer>().enabled = Enabled;
+			GetComponent<Image>().enabled = Enabled;
 		}
 	}
 	
 	public void setVisible(bool value) {
 		visible = value;
-		GetComponent<SpriteRenderer>().enabled = Enabled;
+		GetComponent<Image>().enabled = Enabled;
 	}
 	
 	public bool Enabled {
